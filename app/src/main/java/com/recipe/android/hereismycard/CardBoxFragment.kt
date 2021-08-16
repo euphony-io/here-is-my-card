37f14f63dab787507d59ce257a95d80292c48269
Suyeon Jang <mmmm07@naver.com>
2021-08-17 00:52:51 +0900

    Add Activities : MainActivity, MakeCardFragment, CardBoxFragment

---
+++ app/src/main/java/com/recipe/android/hereismycard/CardBoxFragment.kt
@@ -0,0 +1,31 @@
+package com.recipe.android.hereismycard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.recipe.android.hereismycard.databinding.FragmentCardBoxBinding
+
+class CardBoxFragment: Fragment() {
+    private var _binding: FragmentCardBoxBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding =FragmentCardBoxBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
