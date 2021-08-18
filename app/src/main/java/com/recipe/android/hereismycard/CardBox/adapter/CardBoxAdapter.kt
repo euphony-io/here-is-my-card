db53f3d8bf75da2266409dd56489711eb16de1a2
SeongHo Park <psh2849@gmail.com>
2021-08-18 17:27:13 +0900

    Fetch all cards from Database

--- app/src/main/java/com/recipe/android/hereismycard/CardBox/adapter/CardBoxAdapter.kt
+++ app/src/main/java/com/recipe/android/hereismycard/CardBox/adapter/CardBoxAdapter.kt
@@ -1,32 +1,43 @@
 package com.recipe.android.hereismycard.CardBox.adapter
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.recipe.android.hereismycard.CardBox.db.Card
+import com.recipe.android.hereismycard.MainActivity
 import com.recipe.android.hereismycard.databinding.ItemCardBoxBinding
 
-class CardBoxAdapter: RecyclerView.Adapter<CardBoxAdapter.CardBoxViewHolder>() {
-    inner class CardBoxViewHolder(val binding: ItemCardBoxBinding): RecyclerView.ViewHolder(binding.root){
-        fun bindWithView(position: Int) {
-
+class CardBoxAdapter(
+    var cardList: ArrayList<Card>
+) : RecyclerView.Adapter<CardBoxAdapter.CardBoxViewHolder>() {
+    inner class CardBoxViewHolder(val binding: ItemCardBoxBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bindWithView(card: Card) {
+            binding.etName.text = card.name
+            binding.etTel.text = card.tel
+            binding.etEmail.text = card.email
+            binding.etAddress.text = card.address
+            binding.cardBg.setBackgroundResource(MainActivity.bgList[card.bg])
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardBoxViewHolder {
-        return CardBoxViewHolder(ItemCardBoxBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return CardBoxViewHolder(
+            ItemCardBoxBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: CardBoxViewHolder, position: Int) {
-        holder.bindWithView(position)
+        holder.bindWithView(cardList[position])
     }
 
-    // 데이터 받기 전 임시 카운트
-    // 데이터 listen 기능 구현 후 수정해주세요.
-    override fun getItemCount(): Int = 5
-
-    fun submitList(){
-
-    }
-
-
+    override fun getItemCount(): Int = cardList.size
 }
