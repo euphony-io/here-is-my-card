773e0d63f645807fc0dc1c16d39f7e8d8c970f8c
Suyeon Jang <mmmm07@naver.com>
2021-08-21 19:33:38 +0900

    Fix App Crash using Activity not null check

--- app/src/main/java/com/recipe/android/hereismycard/makeCard/MakeCardFragment.kt
+++ app/src/main/java/com/recipe/android/hereismycard/makeCard/MakeCardFragment.kt
@@ -129,7 +129,8 @@ class MakeCardFragment : Fragment() {
             db.cardDao().insertCard(card)
         }).start()
 
-        Toast.makeText(activity, "SAVE CARD SUCCESS!", Toast.LENGTH_LONG).show()
+        if (activity != null)
+            Toast.makeText(requireActivity(), "SAVE CARD SUCCESS!", Toast.LENGTH_LONG).show()
     }
 
     private fun stopListen() {
