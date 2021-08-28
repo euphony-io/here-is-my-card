09255bcad7d6486180e91469a9e83cb47bd7084a
parksh <parksh5061@naver.com>
2021-08-28 18:16:35 +0900

    Add LongClick Listener

--- app/src/main/java/com/recipe/android/hereismycard/CardBox/adapter/CardBoxAdapter.kt
+++ app/src/main/java/com/recipe/android/hereismycard/CardBox/adapter/CardBoxAdapter.kt
@@ -1,11 +1,8 @@
 package com.recipe.android.hereismycard.CardBox.adapter
 
-import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.recipe.android.hereismycard.CardBox.db.Card
 import com.recipe.android.hereismycard.MainActivity
@@ -14,6 +11,7 @@ import com.recipe.android.hereismycard.databinding.ItemCardBoxBinding
 class CardBoxAdapter(
     var cardList: ArrayList<Card>
 ) : RecyclerView.Adapter<CardBoxAdapter.CardBoxViewHolder>() {
+    private lateinit var itemClickListener : OnItemClickListener
     inner class CardBoxViewHolder(val binding: ItemCardBoxBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bindWithView(card: Card) {
@@ -24,7 +22,13 @@ class CardBoxAdapter(
             binding.cardBg.setBackgroundResource(MainActivity.bgList[card.bg])
         }
     }
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
 
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardBoxViewHolder {
         return CardBoxViewHolder(
             ItemCardBoxBinding.inflate(
@@ -37,6 +41,10 @@ class CardBoxAdapter(
 
     override fun onBindViewHolder(holder: CardBoxViewHolder, position: Int) {
         holder.bindWithView(cardList[position])
+        holder.itemView.setOnLongClickListener{
+            itemClickListener.onClick(it, position)
+            true
+        }
     }
 
     override fun getItemCount(): Int = cardList.size
